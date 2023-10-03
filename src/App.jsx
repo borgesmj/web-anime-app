1624a20fa478b40a8b734182c3cdc2f876033804
borgesmj <borgesmj19@gmail.com>
2023-10-03 12:02:54 -0500

    testing Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import Sidebar from './Components/Sidebar'
 import HeroBtn from './Components/HeroBtn'
@@ -6,6 +7,21 @@ import HeroBtn from './Components/HeroBtn'
 function App() {
 
   const [sideBar, setSideBar] = useState(false)
+  const [grid, setGrid] = useState([])
+
+  useEffect(()=>{
+    axios
+    .get('https://borgesmj.github.io/cloud-storage/animedb.json')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+    .finally(function(){
+      console.log('fetch finalizado')
+    })
+  },[])
 
   return (
     <div className='max-w-full py-5'>
