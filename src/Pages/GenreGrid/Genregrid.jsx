08f51cd3c873e8855df66bc08893372cad327cf2
borgesmj <borgesmj19@gmail.com>
2023-10-11 09:33:03 -0500

    Added functionality to load more anime titles when the user clicks the "Load more" button.

--- src/Pages/GenreGrid/Genregrid.jsx
+++ src/Pages/GenreGrid/Genregrid.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import AnimeTitle from '../../Components/AnimeTitle/AnimeTitle'
+import Loader from "../../Components/Loader/Loader";
+import { useLocation } from "react-router-dom";
 
 const Genregrid = ({genre, allAnime}) => {
-  
+  const [maxLength, setMaxLength] = useState(50);
+  const [loading, setLoading] = useState(false);
   const filteredData = allAnime.filter((item) => {
     return item.genres.includes(genre);
   });
+  const location = useLocation;
+  useEffect(() => {
+    setMaxLength(50);
+  }, [location.pathname]);
+
+  useEffect(() => {}, []);
+
+  const handleLoadCLick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setMaxLength(maxLength + 50);
+      setLoading(false);
+    }, 1500);
+  };
   return (
     <div className="absolute top-40 w-full z-[-2]  px-8 md:px-[1rem] md:w-3/5 md:top-20 md:left-[19rem] lg:left-[18rem] lg:w-4/5 py-4  ">
-      <h1 className='w-full  text-center font-bold md:text-xl'>{genre}</h1>
+      <h1 className="w-full  text-center font-bold md:text-xl">{genre}</h1>
       <div className="flex flex-col justify-center items-center md:grid md:grid-cols-2 lg:grid-cols-4">
-        {filteredData.map((item) => (
-          <AnimeTitle name={item.title} image={item.image} />
+        {filteredData.slice(0, maxLength).map((item) => (
+          <AnimeTitle key={`genre_key_${item._id}_${item.title}`} name={item.title} image={item.image} />
         ))}
       </div>
+      {filteredData.length > maxLength ? (
+        <button
+          className="w-full py-4 bg-primary-100 text-xl mt-4 rounded-[50px] text-white font-bold relative h-16"
+          onClick={handleLoadCLick}
+        >
+          {loading ? <Loader /> : "Load more..."}
+        </button>
+      ) : null}{" "}
     </div>
   )
 }
