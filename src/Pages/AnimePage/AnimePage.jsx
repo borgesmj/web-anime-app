671bec6503570c3ce47a0f51ebf5e16e581dd4e7
borgesmj <borgesmj19@gmail.com>
2023-10-11 09:52:50 -0500

    split anime synopsis to render all w/o the author

--- src/Pages/AnimePage/AnimePage.jsx
+++ src/Pages/AnimePage/AnimePage.jsx
@@ -10,7 +10,6 @@ const AnimePage = ({ allAnime }) => {
 
   const animeSynopsis = filteredAnime[0]?.synopsis.split('[')
 
-  console.log(animeSynopsis[0])
   const navigate = useNavigate();
   return (
     <div className="absolute top-40 w-full z-[-2]  px-8 md:px-[1rem] md:w-3/5 md:top-20 md:left-[19rem] lg:left-[18rem] lg:w-4/5 py-4  ">
